547cc4324fc949dde08d6b168f9ebc5c513d3aca
Coleman, Jack <jack.coleman@lexisnexisrisk.com>
2019-10-11 02:21:16 -0400

    remove mute

--- audio.py
+++ audio.py
@@ -1,13 +1,10 @@
 import pygame
 
-mute = False
-
 
 def play(audio_file):
-    if not mute:
-        pygame.init()
-        pygame.mixer.init()
-        pygame.mixer.music.load(audio_file)
-        pygame.mixer.music.play()
-        while pygame.mixer.music.get_busy():
-            continue
+    pygame.init()
+    pygame.mixer.init()
+    pygame.mixer.music.load(audio_file)
+    pygame.mixer.music.play()
+    while pygame.mixer.music.get_busy():
+        continue
