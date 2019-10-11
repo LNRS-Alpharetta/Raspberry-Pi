f10b27381c6e9aed3726bd0704c67ccc95d15fae
Coleman, Jack <jack.coleman@lexisnexisrisk.com>
2019-10-11 10:46:22 -0400

    fix timing of camera

--- main.py
+++ main.py
@@ -22,11 +22,11 @@ while True:
     celeb_labels = []
     # object is triggered by a button push
     if button.is_pressed:
-        audio.play('./mp3/intro_comment.mp3')
         print("working...")
         # capture picture from camera
         with picamera.PiCamera() as camera:
             camera.capture(image_file)
+        audio.play('./mp3/intro_comment.mp3')
         # upload picture to S3
         s3 = storage.upload(image_file)
         # call rekognition apis
