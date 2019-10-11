a6c4652839829f8ffc7cc6e38b0034e9d6552a4d
Coleman, Jack <jack.coleman@lexisnexisrisk.com>
2019-10-11 00:51:22 -0400

    cleanup

---
+++ polly.py
@@ -0,0 +1,19 @@
+import boto3
+import audio
+
+polly = boto3.Session().client('polly')
+voice = 'Joanna'
+file = '/tmp/temp.mp3'
+
+
+def speak(labels):
+    word_string = ""
+    for word in labels:
+        word_string = word_string + ", " + word
+    speech = polly.synthesize_speech(Text=word_string,
+                                     OutputFormat='mp3',
+                                     VoiceId=voice)
+    with open(file, 'wb') as writer:
+        writer.write(speech['AudioStream'].read())
+        writer.close()
+    audio.play(file)
